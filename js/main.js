534743353b7d386bac17e654d1e65025d1dd2f10
Aristocrat4 <galadanelia1@gmail.com>
2021-12-01 22:22:57 +0400

    add click event on mobile menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const navBtn = document.querySelector('.menu-icon');
+const navBar = document.querySelector('.mobile-navigation');
+const transparent = document.querySelector('.transparent');
+navBtn.addEventListener('click', function (){
+    navBar.classList.toggle('appear');
+    transparent.classList.toggle('appear');
+})
+transparent.addEventListener('click', function(){
+    navBar.classList.toggle('appear');
+    transparent.classList.toggle('appear');
+})
